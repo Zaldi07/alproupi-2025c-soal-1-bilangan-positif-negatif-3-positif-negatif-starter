13b5c2b81e22c94e7caff1fba41e379b262b36ec
zaldi nugraha <90915592+Zaldi07@users.noreply.github.com>
2025-09-25 08:44:59 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
-int main() {
-    // Tulis kode Anda di sini
-    return 0;
+int main()
+{
+    int angka;
+
+    printf("Masukan angka :");
+    scanf("%d", &angka);
+    if (angka > 0)
+    {
+        printf("%d adalah bilangan positif", angka);
+    }
+    else if (angka < 0)
+    {
+        printf("%d Angka adalah bilangan negatif", angka);
+    }
+    else
+    {
+        printf("%d adalah bilangan 0", angka);
+    }
 }
